aa02ab5df582606b1ca976703082ac982b8e79ba
Oisin Conolly <oisin@oacdesigns.com>
2015-01-24 17:54:46 +0100

    get block chain data and parse into score for instruments

---
+++ public/getData.php
@@ -0,0 +1,156 @@
+<?php
+
+include('../vendor/autoload.php');
+use Blocktrail\SDK\BlocktrailSDK;
+
+
+$apiKey = "76fbd6319b171d12ab8a87c739e1ee16a9e4ba3a";
+$apiSecret = "70e1654ce7bc6301c3c33108eee1b42a7f698aa2";
+$network = 'BTC';
+$testnet = false;
+$apiVersion = 'v1';
+
+$client = new BlocktrailSDK($apiKey, $apiSecret, $network, $testnet, $apiVersion);
+$client->setCurlDefaultOption('verify', false);
+
+//get the data
+$block = $_GET['block'];
+$data['block'] = $client->block($block);
+$data['transactions'] = $client->blockTransactions($block, $page=1, $limit=500)['data'];
+
+//map of semitones for a hexadec tone conversion (needs 16 tones)
+$toneMap = array(
+    'C',
+    'C#',
+    'D',
+    'Eb',
+    'E',
+    'F',
+    'F#',
+    'G',
+    'G#',
+    'A',
+    'Bb',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+);
+$toneMap = array(
+    'C',
+    'C',
+    'D',
+    'E',
+    'E',
+    'F',
+    'F',
+    'G',
+    'G',
+    'A',
+    'B',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+);
+
+//1. Instruments
+$instruments = array();
+//parse the block data to determine a drum track?
+$instruments[4] = array();
+//...
+
+//take chunks of 4 transactions, to create 4 instruments for melody
+//max of 125 notes
+foreach ($data['transactions'] as $key => $tx) {
+    //parse the transaction to create performance data
+    //1. note (one and pitch)
+    $tone = hexdec(substr($tx['hash'], 0, 1));
+    $note = $toneMap[$tone];
+    $pitch = 4;
+
+    //2. length (convert value to semiquavers)
+    $length = floor($tx['estimated_value']/6250000);
+    //compress length into range of 1 semiquaver to 1 semibreve ()
+    //or...use second part of hash to determin the length
+    $length = hexdec(substr($tx['hash'], 1, 1));
+
+    $performance = array(
+        'note' => $note.$pitch,
+        'value' => $tx['estimated_value'],
+        'length' => $length,
+    );
+
+    //add the performance data to the relevant instrument
+    $instrumentIndex = ($key+1) % 4;
+    $instruments[$instrumentIndex][] = $performance;
+}
+
+//parse the block data to determine a drum track?
+$length = count($instruments[0]);
+for ($length; $length>0; $length--) {
+    $performance = array(
+        'note' => 'A1',
+        'value' => 0,
+        'length' => 8,
+    );
+    $instruments[4][] = $performance;
+}
+
+
+//echo json_encode($instruments);
+//exit;
+
+//an array of instruction, each index being an incremented semi-quaver
+$blockchainScore = array();
+
+//for each instrument, read it's score and create instructions (note, length, pitch info, etc) at the correct position
+foreach ($instruments as $instrumentKey => $instrument) {
+    $scorePosition = 0;
+    foreach ($instrument as $key => $instruction) {
+        //put the instruction in the correct place
+        $blockchainScore[$scorePosition][$instrumentKey] = $instruction;
+        //increment the position to the next point 
+        $scorePosition += $instruction['length'];
+    }
+}
+//fill in the blanks - get how may instructions there should be, and create empty instructions for each missing index
+end($blockchainScore);
+$totalInstructions = key($blockchainScore);
+for ($i=0; $i<$totalInstructions; $i++) {
+    if (!isset($blockchainScore[$i])) {
+        $blockchainScore[$i] = array();
+    }
+}
+
+echo json_encode($blockchainScore);
+exit;
+
+echo json_encode($data);
+
+
+
+
+/*
+ Each block is a song. 
+ The transactions are the notes:
+    the hash determines the instruments notes (split up to x chunks, one for each instrument, and then convert into a tone/semitone)
+    the tx value determines the length
+    greater tx value = higher pitch?
+    
+
+Transactions are the notes:
+    take 4 tx at a time - one for each instrument
+    use the value to determine the note length (convert into nearest unit: 1bitcoin = 1 semibreve, 0.5: minim, 0.25:crotchet, 0.125: quaver, 0.0625: semi-quaver)
+    convert the hash into 
+
+
+
+Frontend:
+    1.
+    create array of instructions, with each index being a semi-quaver. instructions in each saying what to do with each instrument (set config, start play, stop play, etc)
+    timeout: reads each index of array and decides what to do
+
+ */
